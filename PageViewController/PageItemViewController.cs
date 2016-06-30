cac5aff1d419068984864f1316277ecadb5160f1
sreenivaskavali <sreenivas.kavaly@gmail.com>
2016-06-30 20:13:23 +0530

    Add files via upload
    UIPageViewController Example.

---
+++ PageViewController/PageItemViewController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using UIKit;
+
+namespace PageViewController
+{
+	public partial class PageItemViewController : UIViewController
+	{
+		public int Index = 0;
+
+		public string ImageName;
+
+		public PageItemViewController(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			if(!string.IsNullOrEmpty(ImageName))
+			  this.imgView.Image = UIImage.FromBundle(ImageName);
+
+			// pich on image
+			scrollView.MaximumZoomScale = 3f;
+			scrollView.MinimumZoomScale = 1f;
+			scrollView.ViewForZoomingInScrollView += (UIScrollView sv) => { return imgView; };
+			scrollView.ShowsVerticalScrollIndicator = false;
+			scrollView.ShowsVerticalScrollIndicator = false;
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
+
---
+++ PageViewController/ViewController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UIKit;
+
+namespace PageViewController
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public List<string> contentImages = null;
+		UIStoryboard storyboardToUse = null;
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			try
+			{
+				storyboardToUse = UIStoryboard.FromName("Main", null);
+				contentImages = new List<string>() { "scenery1.jpg", "scenery2.jpg", "scenery3.jpg", "scenery4.jpg", "scenery5.jpg" };
+
+				var pageViewController = (PageViewController)storyboardToUse.InstantiateViewController("PageViewControllerSBID");
+				pageViewController.DataSource = new PageDataSource(this);
+
+				var firstView = GetItemController(0);
+				pageViewController.SetViewControllers(new[] { firstView }, UIPageViewControllerNavigationDirection.Forward, true, null);
+
+				this.AddChildViewController(pageViewController);
+				this.View.AddSubview(pageViewController.View);
+				pageViewController.DidMoveToParentViewController(this);
+
+				SetupPageControl();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
+
+
+		private void SetupPageControl()
+		{
+			var appearance = UIPageControl.Appearance;
+			appearance.PageIndicatorTintColor = UIColor.Gray;
+			appearance.CurrentPageIndicatorTintColor = UIColor.White;
+			appearance.BackgroundColor = UIColor.DarkGray;
+		}
+
+
+		public PageItemViewController GetItemController(int index)
+		{
+
+			if (index < contentImages.Count)
+			{
+				var pageItemController = (PageItemViewController)storyboardToUse.InstantiateViewController("PageItemViewControllerSBID");
+
+				pageItemController.Index = index;
+				pageItemController.ImageName = contentImages[index];
+				return pageItemController;
+			}
+
+			return null;
+		}
+	}
+
+
+	public class PageDataSource : UIPageViewControllerDataSource
+	{
+
+		ViewController controller = null;
+
+		public PageDataSource(ViewController controller)
+		{
+			this.controller = controller;
+		}
+
+		override public UIViewController GetPreviousViewController(UIPageViewController pageViewController, UIViewController referenceViewController)
+		{
+			var currentPage = referenceViewController as PageItemViewController;
+
+			if (currentPage.Index > 0 ){
+				
+				return controller.GetItemController(currentPage.Index - 1);
+		    }
+
+			return null;
+		}
+
+		override public UIViewController GetNextViewController(UIPageViewController pageViewController, UIViewController referenceViewController)
+		{
+			var itemController = referenceViewController as PageItemViewController;
+			if (itemController.Index + 1 < controller.contentImages.Count)
+			{
+				return controller.GetItemController(itemController.Index + 1);
+			}
+
+			return null;
+		}	
+
+		public override nint GetPresentationCount(UIPageViewController pageViewController)
+		{
+			return controller.contentImages.Count;
+		}
+
+		public override nint GetPresentationIndex(UIPageViewController pageViewController)
+		{
+			return 0;
+		}
+	}
+}
+
